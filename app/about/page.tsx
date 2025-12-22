c5b1f564b5e479c51547984bdb62a2518be0b4a9
DeAndria Ekong <deandria.ekong@lc.cuny.edu>
2025-12-22 17:18:03 -0500

    Add about page route

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+      <main className="min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-semibold">
+          About This Project
+        </h1>
+      </main>
+    );
+  }
+  
